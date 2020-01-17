6659348b2ee4088e309d1a6515320174d9f6be21
elodieOc <elodie.ollivier.pro@gmail.com>
2020-01-17 17:12:55 +0100

    ajout et correction messages d'erreurs

--- microservice-UI/src/main/java/com/mUI/microserviceUI/exceptions/CustomErrorDecoder.java
+++ microservice-UI/src/main/java/com/mUI/microserviceUI/exceptions/CustomErrorDecoder.java
@@ -18,15 +18,13 @@ public class CustomErrorDecoder implements ErrorDecoder {
         try{
             ApiError error = mapper.readValue(reponse.body().asInputStream(), ApiError.class);
             switch (error.getMessage()){
-                case "Merchant01":
-                    return new BadLoginPasswordException("L'utilisateur n'existe pas");
-                case "Merchant02":
+                case "NotFound":
+                    return new BadLoginPasswordException("L'item demandé n'existe pas");
+                case "LoginPassword":
                     return new BadLoginPasswordException("L'association login/mot de passe est invalide");
-                case "Merchant03":
-                    return new CannotAddException("Cet email est déjà lié à un compte");
-                case "Merchant04":
-                    return new CannotAddException("Ce nom est déjà lié à un compte");
-                case "Merchant05":
+                case "UniqueFail":
+                    return new CannotAddException("Existe déjà en base de donnée");
+                case "AddFail":
                     return new CannotAddException("Echec dans l'ajout");
                 default: return defaultErrorDecoder.decode(invoqueur, reponse);
             }
