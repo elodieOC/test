e4ae8f209faba703fe6d581f88e5897a995923bf
elodieOc <elodie.ollivier.pro@gmail.com>
2020-01-31 14:49:20 +0100

    ajout show/hide mot de passe dans la page login

---
+++ microservice-UI/src/main/resources/static/js/togglepassword.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#password-eye").on('click', function(event) {
+        event.preventDefault();
+        if($('#password').attr("type") == "text"){
+            $('#password').attr('type', 'password');
+            $('#eye-icon').removeClass( "glyphicon glyphicon-eye-open" );
+            $('#eye-icon').addClass( "glyphicon glyphicon-eye-close" );
+        }else if($('#password').attr("type") == "password"){
+            $('#password').attr('type', 'text');
+            $('#eye-icon').removeClass( "glyphicon glyphicon-eye-close" );
+            $('#eye-icon').addClass( "glyphicon glyphicon-eye-open" );
+        }
+    });
+});
